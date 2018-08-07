fdd6630916a85f495fb45ef5443bd8f614307e75
Ronil Bhatia <ronilbhatia@gmail.com>
2018-08-07 12:10:52 -0700

    Add User model

--- Gemfile
+++ Gemfile
@@ -23,7 +23,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+
+gem 'annotate'
+
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
@@ -46,6 +49,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
 end
 
 group :test do
---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_niL: true }
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username) || User.find_by(email: email)
+    return nil unless user
+    @user.is_password?(password) ? @user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+
+end

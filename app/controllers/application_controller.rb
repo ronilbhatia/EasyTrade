d40222056c744c94cbcda1bf9cd947c807e47a52
Ronil Bhatia <ronilbhatia@gmail.com>
2018-08-07 13:48:14 -0700

    Add Application controller functions to login and logout users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  # protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
 end

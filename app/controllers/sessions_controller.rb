130974b5cd07ac10eb20e67c75c59d317f9313bc
Ronil Bhatia <ronilbhatia@gmail.com>
2018-08-07 13:55:06 -0700

    Add Sessions Controller and finish back end auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login!(@user)
+      render json: @user
+    else
+      render json: ['Invalid Credentials'], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else
+      render json: {}, status: 404
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
 end

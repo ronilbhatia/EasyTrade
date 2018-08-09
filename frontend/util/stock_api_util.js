e170412ed7dfa2f1ea16217157ef11b72bc29280
Ronil Bhatia <ronilbhatia@gmail.com>
2018-08-08 22:17:44 -0700

    Update session form styling to make more dynamic to screen size and add input:focus styling

---
+++ frontend/util/stock_api_util.js
@@ -0,0 +1,6 @@
+const getStockDailyData = ticker => $.ajax({
+    method: 'get',
+    url: 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=8HKZIEMV1YT01X2A'
+});
+
+// .then(res => console.log(res["Time Series (Daily)"]['2018-08-08']))

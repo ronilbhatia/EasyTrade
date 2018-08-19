3c309ee393c921c51d8e7652cb453bcd867c89b0
Ronil Bhatia <ronilbhatia@gmail.com>
2018-08-18 23:57:43 -0700

    Update news api util to get more relevant news for home page

--- frontend/components/stocks/stock_news_item.jsx
+++ frontend/components/stocks/stock_news_item.jsx
@@ -12,7 +12,7 @@ const StockNewsItem = ({ newsItem }) => {
       <a href={newsItem.url} className="stock-news-item">
         <img src={newsItem.urlToImage} />
         <main>
-          <h4>{sources[newsItem.source.name]}</h4>
+          <h4>{newsItem.source.name}</h4>
           <h3>{newsItem.title}</h3>
           <p>{newsItem.description}</p>
         </main>
--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,5 +1,5 @@
 export const fetchNews = () => (
   $.ajax({
-    url: 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f0b1d8074e784145b023467d4fc9c649&language=en&domains=wsj.com,nytimes.com,seekingalpha.com,yahoo.com'
+    url: 'https://newsapi.org/v2/top-headlines?apiKey=f0b1d8074e784145b023467d4fc9c649&language=en&sources=bloomberg,business-insider,cnbc'
   })
 );

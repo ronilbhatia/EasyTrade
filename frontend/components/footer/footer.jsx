e02eb9f93127828f0cab2d7c60b3b5edcac648f4
Ronil Bhatia <ronilbhatia@gmail.com>
2018-10-13 17:16:26 -0700

    Modify git logo to match color of other logos

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => (
   <div className='footer'>
     <a href="mailto:ronilbhatia@gmail.com"><img src={window.images.email} /></a>
-    <a href="https://github.com/ronilbhatia"><img className="github" src={window.images.github} /></a>
+    <a href="https://github.com/ronilbhatia"><img className="github" src={window.images.github2} /></a>
     <a href="https://www.linkedin.com/in/ronilbhatia/"><img src={window.images.linkedin} /></a>
   </div>
 );

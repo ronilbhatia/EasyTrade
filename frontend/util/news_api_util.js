c3c0b42b19308f053e58571606c5bed2d8d78847
Ronil Bhatia <ronilbhatia@gmail.com>
2018-10-14 16:11:29 -0700

    Update News API Util call to render less blank images

--- frontend/util/news_api_util.js
+++ frontend/util/news_api_util.js
@@ -1,5 +1,5 @@
 export const fetchNews = () => (
   $.ajax({
-    url: 'https://newsapi.org/v2/top-headlines?apiKey=f0b1d8074e784145b023467d4fc9c649&language=en&sources=bloomberg,business-insider,cnbc'
+    url: 'https://newsapi.org/v2/top-headlines?apiKey=f0b1d8074e784145b023467d4fc9c649&language=en&category=business&country=us'
   })
 );

a74e6e648dacab95adf6f1cbd5d97770c192d790
Ronil Bhatia <ronilbhatia@gmail.com>
2019-12-30 13:38:55 -0800

    Remove store and actions from window

--- frontend/easy_trade.jsx
+++ frontend/easy_trade.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/Root';
 import configureStore from './store/store';
-import { fetchStockInfo } from './util/stock_api_util';
-import { fetchUserInfo } from './actions/session_actions';
-import { createTransaction } from './actions/transaction_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -27,12 +24,5 @@ document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchStockInfo = fetchStockInfo;
-  window.createTransaction = createTransaction;
-  window.fetchUserInfo = fetchUserInfo;
-  window.state = store.getState();
-  // window.currentUser = state.users[state.session.id];
   ReactDOM.render(<Root store={store} />, root);
 });
